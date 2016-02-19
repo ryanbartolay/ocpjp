24a6d0dc06f1c36d205427a2adec5a3d97bd94a1
Ryan Kristoffer Bartolay <bartolay.ryan@gmail.com>
2016-02-19 09:23:51 +0800

    enthuware day 7

--- OCP Review/standard_test/com/ryan/standard/test3/NUMBER53.java
+++ OCP Review/standard_test/com/ryan/standard/test3/NUMBER53.java
@@ -1,12 +1,13 @@
 package com.ryan.standard.test3;
 
 import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveAction;
 import java.util.concurrent.RecursiveTask;
 
-public class NUMBER23 extends RecursiveTask<Integer> {
+public class NUMBER53 extends RecursiveTask<Integer> {
 	int[] ia; int from; int to;
 	static final int THRESHOLD = 3;
-	public NUMBER23(int[] ia, int from, int to) {
+	public NUMBER53(int[] ia, int from, int to) {
 		this.ia = ia; 
 		this.from = from; 
 		this.to = to;
@@ -20,8 +21,18 @@ public class NUMBER23 extends RecursiveTask<Integer> {
 	public static void main(String[] args) {
 		int ia[] = new int[]{ 1, 2, 3, 4 , 5, 6, 7};
 		ForkJoinPool fjp = new ForkJoinPool(); 
-		NUMBER23 st = new NUMBER23(ia, 0, 6);  
+		NUMBER53 st = new NUMBER53(ia, 0, 6);  
 		int sum = fjp.invoke(st); 
 		System.out.println("sum = "+sum);
 	}
+}
+
+class NUMBER53Action extends RecursiveAction {
+
+	@Override
+	protected void compute() {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
