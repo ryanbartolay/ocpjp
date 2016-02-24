d4a0f8bb3c95bc5515402ade2caf69431fe6be95
Ryan Kristoffer Bartolay <bartolay.ryan@gmail.com>
2016-02-24 14:33:30 +0800

    enthuware day 10

--- OCP Review/standard_test/com/ryan/standard/test5/Number37.java
+++ OCP Review/standard_test/com/ryan/standard/test5/Number37.java
@@ -6,11 +6,20 @@ import java.util.Deque;
 public class Number37 {
 	public static void main(String[] args) {
 		Deque<Integer> d = new ArrayDeque<>(); 
-		d.add(1); 
-		d.addFirst(2); 
-		d.pop(); 
-		d.offerFirst(3);
+		d.add(1);
+		System.out.println(d);
 		
+		d.addFirst(2); // add in front
+		System.out.println(d);
+		
+		System.out.println(d.pop()); 		
+		System.out.println(d);
+		
+		System.out.println(d.pollFirst());
+		System.out.println(d);
+		
+		
+		d.offerFirst(3); // the same with addFirst
 		System.out.println(d);
 	}
 }
